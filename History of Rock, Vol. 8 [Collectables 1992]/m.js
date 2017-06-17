4404695279f99be2652d6cd0ba41612262a8995e
littleflute <13699175164@sina.cn>
2017-06-17 16:19:14 -0500

    Add files via upload

---
+++ History of Rock, Vol. 8 [Collectables 1992]/m.js
@@ -0,0 +1,16 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "01 Denise.mp3";		
+f[2]	= "02 Do You Believe in Magic-.mp3";		
+f[3]	= "03 This Time.mp3";		
+f[4]	= "04 She'd Rather Be With Me.mp3";		
+f[5]	= "05 Double Shot (Of My Baby's Love).mp3";		
+f[6]	= "06 Long Lonely Nights.mp3";		
+f[7]	= "07 Love Makes the World Go 'Round.mp3";		
+f[8]	= "08 Where or When.mp3";		
+f[9]	= "09 Mission Bell.mp3";		
+f[10]	= "10 I Remember You.mp3";		
+f[11]	= "11 Simon Says.mp3";		
+f[12]	= "12 Diamonds and Pearls.mp3";		
+f[13]	= "13 Liar Liar.mp3";		
+f[14]	= "14 Stranger on the Shore.mp3";		
