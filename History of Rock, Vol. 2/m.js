9bc2e074fef3912f3bb355bbd5bd7b9453c9adf9
littleflute <13699175164@sina.cn>
2017-06-19 16:11:08 -0500

    Add files via upload

---
+++ History of Rock, Vol. 2/m.js
@@ -0,0 +1,16 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "01 Runaround Sue.mp3";		
+f[2]	= "02 Tonite, Tonite.mp3";		
+f[3]	= "03 My True Story.mp3";		
+f[4]	= "04 Hey Little Girl.mp3";		
+f[5]	= "05 Last Kiss.mp3";		
+f[6]	= "06 Every Beat of My Heart.mp3";		
+f[7]	= "07 Duke of Earl.mp3";		
+f[8]	= "08 Let's Stomp.mp3";		
+f[9]	= "09 Five O'Clock World.mp3";		
+f[10]	= "10 Ya Ya.mp3";		
+f[11]	= "11 Whispering Bells.mp3";		
+f[12]	= "12 You Baby.mp3";		
+f[13]	= "13 Once Upon a Time.mp3";		
+f[14]	= "14 Get a Job.mp3";		
